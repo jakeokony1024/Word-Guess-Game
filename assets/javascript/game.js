7fe4b1e8db8825377121ec2d669b28d5cd77734d
Jakeokony1024 <jakeokony@gmail.com>
2019-12-22 15:32:52 -0600

    added doc ready function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,9 @@
-$(document).ready(function() {
-
-    
-
-    })
 
 var answers = ["Frank", "Sweet Dee", "Golden God", "Charlie", "Toe Knife", "Kitten Mittens"];
 var guesses = [""];
 var lettersGuessed = [""];
 var wins = [""];
 var losses = [""];
-});
+
+$(document).ready(function() {});
+
