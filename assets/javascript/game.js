0d3423bfff977fcee1c4f90a1eff6944660f9d79
Jakeokony1024 <jakeokony@gmail.com>
2019-12-22 15:28:27 -0600

    created vaiables for answers, guesses, letters, wins, losses

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+    
+
+    })
+
+var answers = ["Frank", "Sweet Dee", "Golden God", "Charlie", "Toe Knife", "Kitten Mittens"];
+var guesses = [""];
+var lettersGuessed = [""];
+var wins = [""];
+var losses = [""];
+});
