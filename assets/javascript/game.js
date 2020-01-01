129005514487cf5d3057f2922a82659de9da369c
Jakeokony1024 <jakeokony@gmail.com>
2020-01-01 11:54:34 -0600

    console logged answer

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,34 @@
 //Global Variables//
 //-----------------------------------------------------------------------------------------------------//
-var answers = ["Frank", "Sweet Dee", "Golden God", "Charlie", "Toe Knife", "Kitten Mittens"];
-var guesses = [""];
-var lettersGuessed = [""];
-var wins = [""];
-var losses = [""];
+
+//All Arrays//
+var answers = ["frank", "sweet dee", "golden god", "charlie", "toe knife", "kitten mittens"];
 var images = ["../Word-Guess-Game/assets/images/Charlie_kelly.jpg", "../Word-Guess-Game/assets/images/frankReynolds.jpg", "../Word-Guess-Game/assets/images/goldenGod.jpg",
-"../Word-Guess-Game/assets/images/Kitten-mittons.jpg", "../Word-Guess-Game/assets/images/sweetDee.jpg", "../Word-Guess-Game/assets/images/toe-knife.jpg"];
+    "../Word-Guess-Game/assets/images/Kitten-mittons.jpg", "../Word-Guess-Game/assets/images/sweetDee.jpg", "../Word-Guess-Game/assets/images/toe-knife.jpg"
+]
+var selectedWord = [""];
+var lettersinWord = [];
+var blanks = 0;
+var blanksandSucesses = [];
+var wrongletters = [];
+
+//Game counters//
+
+var winCounter = 0;
+var lossCounter = 0;
+var guessesLeft = 10;
+
 //Funtions//
 //-----------------------------------------------------------------------------------------------------//
 
-//Main Process//
-//-----------------------------------------------------------------------------------------------------//
- 
+function startGame (){
 
+    selectedWord = answers[Math.floor(Math.random() * answers.length)];
+    lettersinWord = selectedWord.split("");
+    //Testing funtion//
+    console.log(selectedWord);
+}
+
+//Main Process//
+
+startGame ();
