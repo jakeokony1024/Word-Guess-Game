ec5d4a8b35314b7d3157977953d602ec4df76b2d
Jakeokony1024 <jakeokony@gmail.com>
2020-01-10 16:56:53 -0600

    finalization

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,7 @@
 //-----------------------------------------------------------------------------------------------------//
 
 //All Arrays//
-var answers = ["frank", "sweet dee", "golden god", "charlie", "toe knife", "kitten mittens"];
+var answers = ["frank", "sweet dee", "dennis", "charlie", "mac", "kitten mittens"];
 var images = ["../Word-Guess-Game/assets/images/Charlie_kelly.jpg", "../Word-Guess-Game/assets/images/frankReynolds.jpg", "../Word-Guess-Game/assets/images/goldenGod.jpg",
     "../Word-Guess-Game/assets/images/Kitten-mittons.jpg", "../Word-Guess-Game/assets/images/sweetDee.jpg", "../Word-Guess-Game/assets/images/toe-knife.jpg"
 ]
@@ -89,7 +89,7 @@ function roundComplete(){
         alert("You Won Jabroni!");
         document.getElementById("winCounter").innerHTML = winCounter;
         var audioElement = document.createElement("audio");
-        audioElement.setAttribute("src", "../assets/MP3/onYourBike.mp3");
+        audioElement.setAttribute("src", "./assets/MP3/onYourBike.mp3");
         
         startGame();
     }
@@ -109,6 +109,7 @@ function roundComplete(){
 startGame();
 
 document.onkeyup = function (event) {
+    
     var letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
     checkLetters(letterGuessed);
     
